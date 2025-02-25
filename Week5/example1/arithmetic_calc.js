3f352c2b83e55405e528adcd31dd54105e700cf5
Emmanuel Onu <emmanuel.onu@robogarden.ca>
2025-02-24 20:07:39 -0700

    Arithmetic calc

---
+++ Week5/example1/arithmetic_calc.js
@@ -0,0 +1,11 @@
+const add = function(val1, val2){
+    return val1 + val2;
+}
+
+const sub = function(val1, val2){
+    return val1 - val2;
+}
+
+let module_name = 'Arithmetic Calculations';
+
+module.exports = {add, sub, module_name};
