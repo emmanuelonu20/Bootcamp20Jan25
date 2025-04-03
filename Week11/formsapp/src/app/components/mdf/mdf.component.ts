b41e9883ad61e6ef35003d67ba58c7115ac16487
Emmanuel Onu <emmanuel.onu@robogarden.ca>
2025-04-02 18:11:06 -0600

    Forms in angular

---
+++ Week11/formsapp/src/app/components/mdf/mdf.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-mdf',
+  standalone: false,
+  templateUrl: './mdf.component.html',
+  styleUrl: './mdf.component.css'
+})
+export class MdfComponent {
+
+  userForm;
+
+  constructor(private fb: FormBuilder){
+    this.userForm = fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: [''],
+      phone: ['']
+    });
+  }
+
+  onSubmit(){
+    console.log(this.userForm.value);
+  }
+
+  //Getter methods
+
+  get nameFormControl() {
+    return this.userForm.get('name')!; //Name form control
+  }
+
+  get emailFormControl() {
+    return this.userForm.get('email')!; //Email form control
+  }
+
+  get phoneFormControl() {
+    return this.userForm.get('phone')!; //Phone form control
+  }
+
+}
